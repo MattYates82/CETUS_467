6738d555fc3241a397071ff39fc91e8888ad4543
JasonCFisher <fisherjasonc@gmail.com>
2017-10-23 17:16:35 -0400

    Add files via upload

---
+++ CETUS_Items.hpp
@@ -0,0 +1,37 @@
+#include <string>
+#include "CETUS_Room.hpp"
+#include "CETUS_World.hpp"
+
+#ifndef CETUS_ITEM_HPP
+#define CETUS_ITEM_HPP
+
+class Item {
+	
+	private:
+	
+		string	Name;
+		string	Description;
+		string 	roomDescription;
+		int		Power;
+		int		Healing;
+		bool	weapon;
+		bool	roomFeature;
+		bool	collectible;
+		
+	public:
+	
+		Item();
+		Item(string Name, string Description, string roomDesc, int Power, int Healing, bool weapon);
+		~Item();
+		int damageItem(int damage);
+		string getName();
+		string getDescription();
+		string getRoomDescription();
+		int getPower();
+		int getHealing();
+		bool getWeapon();
+		bool getRoomFeature();
+		bool getCollectible();
+	
+};
+#endif
